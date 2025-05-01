f0172dfa8b7f1e494644270727ce1747c7463dd9
luizlealdev <luizandreleal100@gmail.com>
2025-05-01 10:04:26 -0300

    [feat]: implements custom api response in auth controller

--- src/main/java/dev/luizleal/ecommerce/domain/controller/AuthController.java
+++ src/main/java/dev/luizleal/ecommerce/domain/controller/AuthController.java
@@ -2,6 +2,7 @@ package dev.luizleal.ecommerce.domain.controller;
 
 import dev.luizleal.ecommerce.domain.dto.request.LoginDto;
 import dev.luizleal.ecommerce.domain.dto.request.RegisterDto;
+import dev.luizleal.ecommerce.domain.dto.response.ApiResponse;
 import dev.luizleal.ecommerce.domain.dto.response.JwtResponseDto;
 import dev.luizleal.ecommerce.domain.dto.response.UserResponseDto;
 import dev.luizleal.ecommerce.domain.service.AuthService;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import static dev.luizleal.ecommerce.persistence.common.ApiResponseStatus.CREATED;
+import static dev.luizleal.ecommerce.persistence.common.ApiResponseStatus.SUCCESS;
+
 @RestController
 public class AuthController {
 
@@ -21,23 +25,47 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/auth/register")
-    public ResponseEntity<UserResponseDto> register(@RequestBody @Valid RegisterDto dto) {
+    public ResponseEntity<ApiResponse<UserResponseDto>> register(
+            @RequestBody @Valid RegisterDto dto
+    ) {
         var createdUser = authService.register(dto);
+        var response = new ApiResponse<>(
+                CREATED.statusCode,
+                CREATED.status,
+                "User created successfully",
+                createdUser
+        );
 
-        return ResponseEntity.status(201).body(createdUser);
+        return ResponseEntity.status(201).body(response);
     }
 
     @PostMapping("/auth/login")
-    public ResponseEntity<UserResponseDto> login(@RequestBody @Valid LoginDto dto) {
+    public ResponseEntity<ApiResponse<UserResponseDto>> login(
+            @RequestBody @Valid LoginDto dto
+    ) {
         var userLogged = authService.login(dto);
+        var response = new ApiResponse<>(
+                SUCCESS.statusCode,
+                SUCCESS.status,
+                "User logged successfully",
+                userLogged
+        );
 
-        return ResponseEntity.ok(userLogged);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/auth/refresh")
-    public ResponseEntity<JwtResponseDto> refreshToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) {
+    public ResponseEntity<ApiResponse<JwtResponseDto>> refreshToken(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization
+    ) {
         var authTokens = authService.refresh(authorization);
+        var response = new ApiResponse<>(
+                SUCCESS.statusCode,
+                SUCCESS.status,
+                "Access token refreshed successfully",
+                authTokens
+        );
 
-        return ResponseEntity.ok(authTokens);
+        return ResponseEntity.ok(response);
     }
 }
