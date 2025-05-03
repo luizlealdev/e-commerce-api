c149e3d31bbcd20e2f4278c617671519330ea5ff
luizlealdev <luizandreleal100@gmail.com>
2025-05-03 18:43:30 -0300

    [refactor]: change class name of the exception that is thrown when the email is already in the database

--- src/main/java/dev/luizleal/ecommerce/exception/EmailAlreadyUsedException.java
+++ src/main/java/dev/luizleal/ecommerce/exception/EmailAlreadyUsedException.java
@@ -3,12 +3,12 @@ package dev.luizleal.ecommerce.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 
-public class UserAlreadyExistsException extends ECommerceException{
+public class EmailAlreadyUsedException extends ECommerceException{
     @Override
     public ProblemDetail toProblemDetail() {
         var pb = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
-        pb.setTitle("User Already Exists");
-        pb.setDetail("The user with this email has already been registered");
+        pb.setTitle("Email already used");
+        pb.setDetail("A user with this email has already been registered");
 
         return pb;
     }
