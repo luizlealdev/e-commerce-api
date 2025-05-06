36bae3fe5f1ce19b748590121cc62c76ee829ac9
luizlealdev <luizandreleal100@gmail.com>
2025-05-06 17:06:37 -0300

    [feat]: create items sold quantity property & and trigger for change product status

--- src/main/java/dev/luizleal/ecommerce/persistence/entity/Product.java
+++ src/main/java/dev/luizleal/ecommerce/persistence/entity/Product.java
@@ -1,6 +1,6 @@
 package dev.luizleal.ecommerce.persistence.entity;
 
-import dev.luizleal.ecommerce.persistence.common.Status;
+import dev.luizleal.ecommerce.persistence.common.ProductStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,15 +32,34 @@ public class Product {
     @Column(name = "quantity")
     private int quantity = 0;
 
+    @Column(name = "items_sold")
+    private int itemsSold = 0;
+
     @Column(name = "created_at")
     @CurrentTimestamp
     private Instant createdAt;
 
     @Column(name = "status")
-    private Status status;
+    private ProductStatus status;
 
     @ManyToOne
     @JoinColumn(name = "seller_id", nullable = false)
     private User seller;
 
+    @PrePersist
+    @PreUpdate
+    public void updateStatusBasedOnQuantity() {
+        if (quantity == 0) {
+            this.status = ProductStatus.OUT_OF_STOCK;
+        } else if (status == ProductStatus.OUT_OF_STOCK) {
+            this.status = ProductStatus.ACTIVE;
+        }
+    }
+
+    public Product(String name, String description, String imageUrl, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.quantity = quantity;
+    }
 }
