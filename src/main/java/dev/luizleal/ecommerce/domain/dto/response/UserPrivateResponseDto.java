d7df97f0e9cc1b1079075c37af6543740fa0138e
luizlealdev <luizandreleal100@gmail.com>
2025-05-07 11:00:16 -0300

    [fix]: add getters and setters in responses dto classes

--- src/main/java/dev/luizleal/ecommerce/domain/dto/response/UserPrivateResponseDto.java
+++ src/main/java/dev/luizleal/ecommerce/domain/dto/response/UserPrivateResponseDto.java
@@ -1,11 +1,15 @@
 package dev.luizleal.ecommerce.domain.dto.response;
 
 import dev.luizleal.ecommerce.persistence.entity.User;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
 import java.util.UUID;
 
+@Getter
+@Setter
 @NoArgsConstructor
 public class UserPrivateResponseDto {
 
